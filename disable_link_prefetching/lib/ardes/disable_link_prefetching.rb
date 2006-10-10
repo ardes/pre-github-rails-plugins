93ec7201a51685ab96f5679e05a1251994becf77
ian <ian@845bbffb-5c18-0410-91b3-f25c072b94c1>
2006-10-10 16:14:30 +0000

    Refactored disable link refetching
    git-svn-id: http://svn.ardes.com/rails_plugins@62 845bbffb-5c18-0410-91b3-f25c072b94c1

--- disable_link_prefetching/lib/ardes/disable_link_prefetching.rb
+++ disable_link_prefetching/lib/ardes/disable_link_prefetching.rb
@@ -6,13 +6,24 @@
 #     before_filter disable_link_prefetching
 #   end
 #
-class ActionController::Base
-private
-  def disable_link_prefetching
-    if request.env["HTTP_X_MOZ"] == "prefetch" 
-      logger.debug "prefetch detected: sending 403 Forbidden" 
-      render_nothing "403 Forbidden" 
-      return false
+module Ardes
+  module DisableLinkPrefetching
+    def self.included(controller)
+      controller.class_eval do
+        include InstanceMethods
+        before_filter :disable_link_prefetching
+      end
+    end
+    
+    module InstanceMethods
+    private
+      def disable_link_prefetching
+        if request.env["HTTP_X_MOZ"] == "prefetch" 
+          logger.debug "prefetch detected: sending 403 Forbidden" 
+          render_nothing "403 Forbidden" 
+          return false
+        end
+      end
     end
   end
 end
