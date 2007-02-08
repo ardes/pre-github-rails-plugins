f652ea319a3437d25c7e62cf7a1d6e49f9257656
ian <ian@845bbffb-5c18-0410-91b3-f25c072b94c1>
2007-02-08 12:57:02 +0000

    Removing old inherit_views
    git-svn-id: http://svn.ardes.com/rails_plugins@171 845bbffb-5c18-0410-91b3-f25c072b94c1

--- inherit_views/init.rb
+++
@@ -1,3 +0,0 @@
-require 'ardes/inherit_views'
-ActionController::Base.class_eval { extend Ardes::ActionController::InheritViews }
-ActionView::Base.class_eval { include Ardes::ActionView::InheritViews }
