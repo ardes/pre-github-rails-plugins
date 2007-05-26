b215be6b2201e34ee9edf962e6019b4960857c1d
ian <ian@845bbffb-5c18-0410-91b3-f25c072b94c1>
2007-05-26 17:25:59 +0000

    test_app: now invokes cruise on plugins individually\n test_app/edge: pistoned rails to r6736
    git-svn-id: http://svn.ardes.com/rails_plugins@268 845bbffb-5c18-0410-91b3-f25c072b94c1

--- test_app/edge/config/environment.rb
+++ test_app/edge/config/environment.rb
@@ -5,7 +5,6 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '1.2.3' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -14,7 +13,7 @@ Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here
   
   # Skip frameworks you're not going to use (only works if using vendor/rails)
-  # config.frameworks -= [ :action_web_service, :action_mailer ]
+  # config.frameworks -= [ :active_resource, :action_mailer ]
 
   # Only load the plugins named here, by default all plugins in vendor/plugins are loaded
   # config.plugins = %W( exception_notification ssl_requirement )
@@ -25,6 +24,13 @@ Rails::Initializer.run do |config|
   # Force all environments to use the same logger level 
   # (by default production uses :info, the others :debug)
   # config.log_level = :debug
+
+  # Your secret key for verifying cookie session data integrity.
+  # If you change this key, all old sessions will become invalid!
+  config.action_controller.session = {
+    :session_key => '_edge_session',
+    :secret      => '_edge_secret'
+  }
 
   # Use the database for sessions instead of the file system
   # (create the session table with 'rake db:sessions:create')
@@ -40,21 +46,9 @@ Rails::Initializer.run do |config|
 
   # Make Active Record use UTC-base instead of local time
   # config.active_record.default_timezone = :utc
-  
+
   # See Rails::Configuration for more options
+  
+  # Application configuration should go into files in config/initializers
+  # -- all .rb files in that directory is automatically loaded
 end
-
-# Add new inflection rules using the following format 
-# (all these examples are active by default):
-# Inflector.inflections do |inflect|
-#   inflect.plural /^(ox)$/i, '\1en'
-#   inflect.singular /^(ox)en/i, '\1'
-#   inflect.irregular 'person', 'people'
-#   inflect.uncountable %w( fish sheep )
-# end
-
-# Add new mime types for use in respond_to blocks:
-# Mime::Type.register "text/richtext", :rtf
-# Mime::Type.register "application/x-mobile", :mobile
-
-# Include your application configuration below
--- test_app/edge/lib/tasks/cruise.rake
+++ test_app/edge/lib/tasks/cruise.rake
@@ -1,4 +1,14 @@
 task :cruise do 
-  ENV['RAILS_ENV'] = 'test'
-  Rake::Task["plugins:rcov:verify"].invoke
+  targets = FileList["#{RAILS_ROOT}/vendor/plugins/*"].exclude('vendor/plugins/*spec*')
+
+  targets.each do |target|
+    if FileList["#{target}/spec/**/*_spec.rb"].size > 0
+      target_name = File.basename(target)
+      puts "\n\n=========\n== Plugin #{target_name}\n"
+      sh("cd #{target}; rake cruise") do |ok,_|
+        failed << target_name unless ok
+      end
+    end
+  end
+  raise "\n==\nThe following targets failed: #{failed.join(', ')}" if failed.size > 0
 end
