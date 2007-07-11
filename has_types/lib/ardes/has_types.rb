4c14c42d61305b9edce3a2c87ded4cd94d639b27
ian <ian@845bbffb-5c18-0410-91b3-f25c072b94c1>
2007-07-11 00:41:03 +0000

    has_types: tiny bugfix - removed relic
    git-svn-id: http://svn.ardes.com/rails_plugins@371 845bbffb-5c18-0410-91b3-f25c072b94c1

--- has_types/lib/ardes/has_types.rb
+++ has_types/lib/ardes/has_types.rb
@@ -15,28 +15,26 @@ module Ardes#:nodoc:
     def has_types(*types)
       raise RuntimeError, "can only specify has_types on an STI base class" unless self == self.base_class
       
-      unless singleton_methods.include?(:subclass_names)
-        self.class_eval do
-          cattr_accessor :type_class_names
-        
-          class<<self
-            # Intercept calls to descend_from_active_record? and load outstanding dependencies, then wipe
-            # all trace of this method intercept
-            def descends_from_active_record_with_has_types?
-              type_class_names.each {|d| require_dependency(d.underscore) }
-              returning descends_from_active_record_without_has_types? do
-                class<<self
-                  alias_method :descends_from_active_record?, :descends_from_active_record_without_has_types?
-                  undef_method :descends_from_active_record_with_has_types?
-                  undef_method :descends_from_active_record_without_has_types?
-                end
+      self.class_eval do
+        cattr_accessor :type_class_names
+      
+        class<<self
+          # Intercept calls to descend_from_active_record? and load outstanding dependencies, then wipe
+          # all trace of this method intercept
+          def descends_from_active_record_with_has_types?
+            type_class_names.each {|d| require_dependency(d.underscore) }
+            returning descends_from_active_record_without_has_types? do
+              class<<self
+                alias_method :descends_from_active_record?, :descends_from_active_record_without_has_types?
+                undef_method :descends_from_active_record_with_has_types?
+                undef_method :descends_from_active_record_without_has_types?
               end
             end
-            alias_method_chain :descends_from_active_record?, :has_types
-              
-            def inherited(child)
-              type_class_names.include?(child.name) ? super : raise(NameError, "#{child.name} is not declared in #{child.base_class.name}. Add has_types :#{child.name.underscore} to #{child.base_class.name} class definition")
-            end
+          end
+          alias_method_chain :descends_from_active_record?, :has_types
+            
+          def inherited(child)
+            type_class_names.include?(child.name) ? super : raise(NameError, "#{child.name} is not declared in #{child.base_class.name}. Add has_types :#{child.name.underscore} to #{child.base_class.name} class definition")
           end
         end
       end
