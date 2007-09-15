103722769b91d288bfc3a257a58fe165cf6544fb
ian <ian@845bbffb-5c18-0410-91b3-f25c072b94c1>
2007-09-15 09:59:09 +0000

    test_app: pistoned rails r7480, rspec r2562
    git-svn-id: http://svn.ardes.com/rails_plugins@435 845bbffb-5c18-0410-91b3-f25c072b94c1

--- test_app/1.2.3/vendor/plugins/rspec/lib/spec/version.rb
+++ test_app/1.2.3/vendor/plugins/rspec/lib/spec/version.rb
@@ -6,8 +6,8 @@
       TINY   = 9
       RELEASE_CANDIDATE = nil
 
-      # RANDOM_TOKEN: 0.729168547494188
-      REV = "$LastChangedRevision: 2561 $".match(/LastChangedRevision: (\d+)/)[1]
+      # RANDOM_TOKEN: 0.070324152014446
+      REV = "$LastChangedRevision: 2562 $".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
       TAG = "REL_#{[MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('_')}".upcase.gsub(/\.|-/, '_')
--- test_app/edge/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
+++ test_app/edge/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
@@ -2,8 +2,8 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.729168547494188
-        REV = "$LastChangedRevision: 2561 $".match(/LastChangedRevision: (\d+)/)[1]
+        # RANDOM_TOKEN: 0.070324152014446
+        REV = "$LastChangedRevision: 2562 $".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
   end
