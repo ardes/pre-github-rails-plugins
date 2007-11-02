4ab1f7d9d072dda183e2cdba076a67372b64db20
ian <ian@845bbffb-5c18-0410-91b3-f25c072b94c1>
2007-11-02 12:02:32 +0000

    has_types: downgrading sexy_migrations in app for bc
    git-svn-id: http://svn.ardes.com/rails_plugins@484 845bbffb-5c18-0410-91b3-f25c072b94c1

--- has_types/spec/app.rb
+++ has_types/spec/app.rb
@@ -3,11 +3,11 @@ Dependencies.load_paths << File.expand_path(File.join(File.dirname(__FILE__), 'f
 ActiveRecord::Migration.suppress_messages do
   ActiveRecord::Schema.define(:version => 0) do
     create_table :animals, :force => true do |t|
-      t.string :type
+      t.column :type, :string
     end
     
     create_table :dwellings, :force => true do |t|
-      t.string :type
+      t.column :type, :string
     end
   end
 end
