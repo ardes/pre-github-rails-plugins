4580bc4bee457af1a48f988b94581fcd745b41f7
ian <ian@845bbffb-5c18-0410-91b3-f25c072b94c1>
2007-11-09 14:29:18 +0000

    updated template
    git-svn-id: http://svn.ardes.com/rails_plugins@494 845bbffb-5c18-0410-91b3-f25c072b94c1

--- template/spec/spec_helper.rb
+++ template/spec/spec_helper.rb
@@ -1,14 +1,13 @@
-ENV["RAILS_ENV"] = "test"
-require File.expand_path(File.join(File.dirname(__FILE__), "../../../../config/environment.rb"))
+# This file is copied to ~/spec when you run 'ruby script/generate rspec'
+# from the project root directory.
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path(File.join(File.dirname(__FILE__), "../../../../config/environment"))
 require 'spec/rails'
 
 Spec::Runner.configure do |config|
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
   config.fixture_path = RAILS_ROOT + '/spec/fixtures'
-  config.before(:each, :behaviour_type => :controller) do
-    raise_controller_errors
-  end
 
   # You can declare fixtures for each behaviour like this:
   #   describe "...." do
